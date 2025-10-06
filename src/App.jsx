ac2d48a98caba7d0aaf65718b9ef1dee4d4107ac
Sabnock <kingsabnock@gmail.com>
2025-10-06 15:53:48 +0800

    Added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landingpage.jsx';
 import Login from './pages/Login.jsx';
 import Homepage from './pages/homepage.jsx';
+import Profile from './pages/profile.jsx';
 import Register from './pages/register.jsx';
 import ForgotPassword from './pages/forgotpass.jsx';
 import ResetPassword from './components/resetpass.jsx';
@@ -12,12 +14,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/recover" element={<ResetPassword />} />
         <Route path="/home" element={<Homepage />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/post-found" element={<PostItem />} />
         <Route path="/claim-item" element={<ClaimItem />} />
       </Routes>
