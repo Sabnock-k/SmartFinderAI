8df6725b627801b377ae79777f5c1db11b438059
Sabnock <kingsabnock@gmail.com>
2025-10-06 16:27:48 +0800

    Minor changes

--- api/utils/db.js
+++ api/utils/db.js
@@ -1,5 +1,5 @@
-import 'dotenv/config';
-import pkg from 'pg';
+import "dotenv/config";
+import pkg from "pg";
 const { Pool } = pkg;
 
 // E change lang ni if mag local dev ka or deploy
@@ -8,7 +8,9 @@ const { Pool } = pkg;
 var localdev = false;
 
 const pool = new Pool({
-  connectionString: localdev ? process.env.LOCAL_DATABASE_URL : process.env.DATABASE_URL,
+  connectionString: localdev
+    ? process.env.LOCAL_DATABASE_URL
+    : process.env.DATABASE_URL,
   ssl: localdev ? false : { rejectUnauthorized: false },
 });
 
