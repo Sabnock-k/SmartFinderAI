9d972b8c2328040de1035430890647f33915dfac
Sabnock <kingsabnock@gmail.com>
2025-10-23 12:59:41 +0800

    Remove rate limiting from login route

--- api/login.js
+++ api/login.js
@@ -1,18 +1,10 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import pool from "./utils/db.js";
-import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
-const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // limit each IP to 10 requests per windowMs
-  standardHeaders: true,
-  legacyHeaders: false,
-});
-
-router.post("/", loginLimiter, async (req, res) => {
+router.post("/", async (req, res) => {
   const { username, password } = req.body;
 
   // basic validation/sanitization
