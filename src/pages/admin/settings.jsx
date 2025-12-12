3e7df49afd138c1633acb4e8f96f955f30863173
Sabnock <kingsabnock@gmail.com>
2025-12-12 18:11:22 +0800

    fixed error : added useEffect

--- src/pages/admin/settings.jsx
+++ src/pages/admin/settings.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import AdminNav from "../../components/admin-nav";
 import useAuth from "../../../api/hooks/useAuth";
